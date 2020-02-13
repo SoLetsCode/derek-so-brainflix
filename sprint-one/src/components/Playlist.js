39dfd917808ebe7e496d9907208b509709ed8c64
Derek <derek@Lilys-MBP.localdomain>
2020-02-13 14:43:14 -0800

    styling finished up to tablet mode, working on desktop

--- sprint-one/src/components/Playlist.js
+++ sprint-one/src/components/Playlist.js
@@ -9,7 +9,7 @@ function Playlist(props) {
       <div className="playlist__card" key={uuidv4()} id={uuidv4()}>
         <img className="playlist__img" src={item.src} alt="beautiful" />
         <div className="playlist__text-container">
-          <span className="playlist__title">{item.title}</span>
+          <span className="playlist__video-title">{item.title}</span>
           <span className="playlist__author">{item.author}</span>
         </div>
       </div>
@@ -18,7 +18,7 @@ function Playlist(props) {
 
   return (
     <div className="playlist">
-      <h2 className="playlist__title">Next Video</h2>
+      <h2 className="playlist__title">NEXT VIDEO</h2>
       {playlistArray}
     </div>
   );
